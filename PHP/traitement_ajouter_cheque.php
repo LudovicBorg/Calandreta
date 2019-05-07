bbd50fd166045a26ae8dadd4eafae53da33e2f57
Chouf05 <34600954+LudovicBorg@users.noreply.github.com>
2019-05-07 17:57:17 +0200

    Cantine OK

---
+++ PHP/traitement_ajouter_cheque.php
@@ -0,0 +1,28 @@
+<?php
+include("../architecture/connexion.php");
+
+	$date_cheque = $_POST['date_cheque'];
+	$numero_cheque = $_POST['numero_cheque'];
+	$montant_cheque = $_POST['montant_cheque'];
+	$parents = $_POST['parents'];
+
+	$sqlinsertcheque = $con->query("INSERT INTO cheques (montant_cheque, numero_cheque, date_cheque, union_parents) VALUES ('$montant_cheque', '$numero_cheque', '$date_cheque', '$parents')");
+
+	$montant_cantine = $con->query("SELECT id_solde, montant FROM solde WHERE union_parents='".$parents."'");
+	$reqmontant_cantine = $montant_cantine->fetch_all();
+	$old_montant = $reqmontant_cantine[0][1];
+
+	$new_montant_cantine = $old_montant + $montant_cheque;
+
+	$sql_nouveau_montant = $con->query("UPDATE solde SET montant='".$new_montant_cantine."' WHERE union_parents='$parents'");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("architecture/head.html"); ?>
+	<link rel="stylesheet" type="text/css" href="CSS/redirection.css">
+</head>
+<body id="body">
+    <?php include("architecture/header.php"); ?>
+    <br />
