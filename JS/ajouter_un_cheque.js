79dd950c4b235df1b374363d031d9a6e6369ff28
Chouf05 <34600954+LudovicBorg@users.noreply.github.com>
2019-05-07 09:27:44 +0200

    Add files via upload

---
+++ JS/ajouter_un_cheque.js
@@ -0,0 +1,14 @@
+function ajouter_un_cheque(){
+  var xmlhttp;
+  xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      alert("La modification a été effectuée");
+    }
+  }
+
+  xmlhttp.open("POST", "PHP/traitement_modifier_prix.php", true);
+  xmlhttp.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
+  xmlhttp.send('prix=' + document.getElementsByName('prix')[0].value);
+}
