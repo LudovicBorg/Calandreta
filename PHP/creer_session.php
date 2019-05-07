b46a344cb88203d87beb1d1845b8e9fab3a1e52f
Chouf05 <34600954+LudovicBorg@users.noreply.github.com>
2019-05-07 09:04:30 +0200

    Add files via upload

--- PHP/creer_session.php
+++ PHP/creer_session.php
@@ -1,7 +1,19 @@
 <?php
-include("architecture/connexion.php");
-else
+//on actualise la session
+session_start();
+// Désactiver le rapport d'erreurs
+error_reporting(0);
+//On vérifie qu'elle soit bien définie
+// if(empty($_SESSION['user']) || empty($_SESSION['password']))
+// {
+//     header('Location: ../index.php');
+//     exit();
+// }
+$con=mysqli_connect("localhost", 'root', 'Toto123' ,"calandreta");
+if(mysqli_connect_errno($con))
 {
+  echo "Erreur de connexion : ".mysqli_connect_error();
+}
 	$nom = $_POST["nom"];
 	$prenom = $_POST["prenom"];
 	$email = $_POST["email"];
@@ -18,7 +30,6 @@ else
 	//On envoie la requête
 	$req = mysqli_query($con, $sql) or die('Erreur SQL !<br>'.mysqli_error($con));
 
-}
 
 //Fermeture de la connexion
 mysqli_close($con);
--- PHP/demarrer_session.php
+++ PHP/demarrer_session.php
@@ -1,7 +1,20 @@
 <?php
-include("architecture/connexion.php");
-else
+//on actualise la session
+session_start();
+// Désactiver le rapport d'erreurs
+error_reporting(0);
+//On vérifie qu'elle soit bien définie
+// if(empty($_SESSION['user']) || empty($_SESSION['password']))
+// {
+//     header('Location: ../index.php');
+//     exit();
+// }
+$con=mysqli_connect("localhost", 'root', 'Toto123' ,"calandreta");
+if(mysqli_connect_errno($con))
 {
+  echo "Erreur de connexion : ".mysqli_connect_error();
+}
+else {
 	$email = $_POST["identifiant"];
 	$password = $_POST["password"];
 		//On sélectionne l'enregistrement correspondant aux identifiants
@@ -31,10 +44,10 @@ else
 		echo "<script language=\"javascript\">";
 		echo "alert('Erreur de connexion, veuillez réessayer !')";
 		echo "</script>";
-		header('Refresh: 0.5; ../index.php');
+		header('Refresh: 5; ../index.php');
 	}
-}
 
+}
 //Fermeture de la connexion
 mysqli_close($con);
 ?>
