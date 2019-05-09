8cec7465cc2dbccfd9b4c282dd7a1c3a0d6d7dfb
Chouf05 <34600954+LudovicBorg@users.noreply.github.com>
2019-05-09 13:54:50 +0200

    Union enfant OK

--- traitement_ajouter_enfant.php
+++ traitement_ajouter_enfant.php
@@ -22,7 +22,7 @@ $req0 = $sql0->fetch_row();
 	//On envoie la requête
 	$reqinsertenfant = mysqli_query($con, $sqlinsertenfant) or die('Erreur SQL !<br>'.mysqli_error($con));
 	//On regarde si id de l'union existe sinon on l'insere
-	$sqlidunion = query("SELECT id_union FROM union_parents WHERE parent1='".$req0[0]."' AND parent2='".$id_parent2."' OR parent1='".$id_parent2."' AND parent2='".$req0[0]."'");
+	$sqlidunion = $con->query("SELECT id_union FROM union_parents WHERE parent1='".$req0[0]."' AND parent2='".$id_parent2."' OR parent1='".$id_parent2."' AND parent2='".$req0[0]."'");
 	$reqidunion = $sqlidunion->fetch_row();
 	if(empty($reqidunion[0])){
 		$sqlinsertunion = ("INSERT INTO union_parents (parent1, parent2) VALUES ('$req0[0]', '$id_parent2')");
